8e978ab69c6e720181be6ce01113d60079c2c721
Abduljelil32 <sheiduhalilu@gmail.com>
2021-12-12 15:06:32 +0100

    Latest update

--- Form1.h
+++ Form1.h
@@ -37,9 +37,11 @@ namespace CppCLRWinformsProjekt {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::TextBox^ txtUrl;
 
-	private: System::Windows::Forms::Label^ label1;
+
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Button^ button3;
+	public: System::Windows::Forms::Button^ button3;
+	private:
+
 	private: System::Windows::Forms::WebBrowser^ webBrowser1;
 	private: System::Windows::Forms::Button^ button4;
 	private: System::Windows::Forms::Button^ button5;
@@ -61,7 +63,6 @@ namespace CppCLRWinformsProjekt {
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->txtUrl = (gcnew System::Windows::Forms::TextBox());
-			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
@@ -72,115 +73,190 @@ namespace CppCLRWinformsProjekt {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(618, 14);
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->button1->Cursor = System::Windows::Forms::Cursors::WaitCursor;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatAppearance->CheckedBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button1->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Red;
+			this->button1->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Black;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->button1->Location = System::Drawing::Point(812, 4);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(30, 27);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"Goto";
+			this->button1->Text = L"@";
 			this->button1->UseVisualStyleBackColor = true;
+			this->button1->UseWaitCursor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
 			// 
 			// txtUrl
 			// 
-			this->txtUrl->Location = System::Drawing::Point(42, 12);
+			this->txtUrl->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->txtUrl->BackColor = System::Drawing::Color::Black;
+			this->txtUrl->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->txtUrl->ForeColor = System::Drawing::Color::White;
+			this->txtUrl->Location = System::Drawing::Point(3, 4);
 			this->txtUrl->Name = L"txtUrl";
-			this->txtUrl->Size = System::Drawing::Size(570, 20);
+			this->txtUrl->Size = System::Drawing::Size(749, 20);
 			this->txtUrl->TabIndex = 1;
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(12, 15);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(32, 13);
-			this->label1->TabIndex = 2;
-			this->label1->Text = L"URL:";
-			this->label1->Click += gcnew System::EventHandler(this, &Form1::label1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(618, 343);
+			this->button2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->button2->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
+			this->button2->Cursor = System::Windows::Forms::Cursors::WaitCursor;
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatAppearance->CheckedBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button2->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Red;
+			this->button2->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Black;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->button2->Location = System::Drawing::Point(748, 4);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->Size = System::Drawing::Size(21, 26);
 			this->button2->TabIndex = 3;
-			this->button2->Text = L"<<Back";
+			this->button2->Text = L"<<";
 			this->button2->UseVisualStyleBackColor = true;
+			this->button2->UseWaitCursor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(618, 71);
+			this->button3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->button3->BackColor = System::Drawing::Color::Transparent;
+			this->button3->Cursor = System::Windows::Forms::Cursors::WaitCursor;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatAppearance->CheckedBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button3->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Red;
+			this->button3->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Black;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::Color::White;
+			this->button3->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button3->Location = System::Drawing::Point(769, 2);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->Size = System::Drawing::Size(18, 26);
 			this->button3->TabIndex = 4;
-			this->button3->Text = L">>Forward";
-			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Text = L">";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->UseWaitCursor = true;
 			this->button3->Click += gcnew System::EventHandler(this, &Form1::button3_Click);
 			// 
 			// webBrowser1
 			// 
-			this->webBrowser1->Location = System::Drawing::Point(12, 38);
+			this->webBrowser1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->webBrowser1->Location = System::Drawing::Point(3, 32);
 			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
 			this->webBrowser1->Name = L"webBrowser1";
-			this->webBrowser1->Size = System::Drawing::Size(600, 400);
+			this->webBrowser1->ScriptErrorsSuppressed = true;
+			this->webBrowser1->Size = System::Drawing::Size(940, 537);
 			this->webBrowser1->TabIndex = 5;
 			this->webBrowser1->Url = (gcnew System::Uri(L"", System::UriKind::Relative));
 			// 
 			// button4
 			// 
-			this->button4->Location = System::Drawing::Point(618, 265);
+			this->button4->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->button4->BackColor = System::Drawing::Color::Transparent;
+			this->button4->Cursor = System::Windows::Forms::Cursors::WaitCursor;
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatAppearance->CheckedBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button4->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Red;
+			this->button4->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Black;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->ForeColor = System::Drawing::Color::White;
+			this->button4->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->button4->Location = System::Drawing::Point(868, 8);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(75, 23);
 			this->button4->TabIndex = 6;
 			this->button4->Text = L"Search";
-			this->button4->UseVisualStyleBackColor = true;
+			this->button4->UseVisualStyleBackColor = false;
+			this->button4->UseWaitCursor = true;
 			this->button4->Click += gcnew System::EventHandler(this, &Form1::button4_Click);
 			// 
 			// button5
 			// 
-			this->button5->Location = System::Drawing::Point(618, 128);
+			this->button5->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->button5->BackColor = System::Drawing::Color::Transparent;
+			this->button5->Cursor = System::Windows::Forms::Cursors::WaitCursor;
+			this->button5->FlatAppearance->BorderSize = 0;
+			this->button5->FlatAppearance->CheckedBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button5->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Red;
+			this->button5->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Black;
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->ForeColor = System::Drawing::Color::White;
+			this->button5->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->button5->Location = System::Drawing::Point(785, 4);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(75, 23);
+			this->button5->Size = System::Drawing::Size(28, 24);
 			this->button5->TabIndex = 7;
-			this->button5->Text = L"Refresh";
-			this->button5->UseVisualStyleBackColor = true;
+			this->button5->Text = L"(  )";
+			this->button5->UseVisualStyleBackColor = false;
+			this->button5->UseWaitCursor = true;
 			this->button5->Click += gcnew System::EventHandler(this, &Form1::button5_Click);
 			// 
 			// button6
 			// 
-			this->button6->Location = System::Drawing::Point(618, 191);
+			this->button6->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->button6->BackColor = System::Drawing::Color::Transparent;
+			this->button6->Cursor = System::Windows::Forms::Cursors::WaitCursor;
+			this->button6->FlatAppearance->BorderSize = 0;
+			this->button6->FlatAppearance->CheckedBackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(255)),
+				static_cast<System::Int32>(static_cast<System::Byte>(128)), static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->button6->FlatAppearance->MouseDownBackColor = System::Drawing::Color::Red;
+			this->button6->FlatAppearance->MouseOverBackColor = System::Drawing::Color::Black;
+			this->button6->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button6->ForeColor = System::Drawing::Color::White;
+			this->button6->ImageAlign = System::Drawing::ContentAlignment::MiddleRight;
+			this->button6->Location = System::Drawing::Point(838, 6);
 			this->button6->Name = L"button6";
-			this->button6->Size = System::Drawing::Size(75, 23);
+			this->button6->Size = System::Drawing::Size(48, 25);
 			this->button6->TabIndex = 8;
 			this->button6->Text = L"Home";
-			this->button6->UseVisualStyleBackColor = true;
+			this->button6->UseVisualStyleBackColor = false;
+			this->button6->UseWaitCursor = true;
 			this->button6->Click += gcnew System::EventHandler(this, &Form1::button6_Click);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
-			this->ClientSize = System::Drawing::Size(705, 561);
+			this->BackColor = System::Drawing::Color::BlueViolet;
+			this->ClientSize = System::Drawing::Size(935, 572);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button6);
 			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->webBrowser1);
-			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
-			this->Controls->Add(this->label1);
 			this->Controls->Add(this->txtUrl);
 			this->Controls->Add(this->button1);
 			this->Name = L"Form1";
-			this->Text = L"Form1";
+			this->Text = L"JelaBrowser";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		webBrowser1->Navigate(txtUrl->Text);
-	}
+		
+					webBrowser1->Navigate(txtUrl->Text);
+			}
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	webBrowser1->GoBack();
 }
@@ -197,6 +273,8 @@ private: System::Void button6_Click(System::Object^ sender, System::EventArgs^ e
 	webBrowser1->GoHome();
 }
 private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
